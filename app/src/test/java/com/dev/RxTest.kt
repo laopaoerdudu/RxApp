6a0969a5bf7cd1b7eb0ddd9e3de945c64ceed182
David <18588744489@163.com>
2021-02-07 21:46:32 +0800

    Update

--- app/src/test/java/com/dev/RxTest.kt
+++ app/src/test/java/com/dev/RxTest.kt
@@ -1,6 +1,7 @@
 package com.dev
 
 import com.dev.OperatorUtils.Companion.logThread
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.functions.Action
@@ -8,9 +9,11 @@ import io.reactivex.functions.BiFunction
 import io.reactivex.functions.Consumer
 import io.reactivex.functions.Function
 import io.reactivex.observables.ConnectableObservable
+import io.reactivex.observers.TestObserver
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.TestScheduler
+import io.reactivex.subscribers.TestSubscriber
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -1025,5 +1028,150 @@ class RxTest {
 
         //测试线程休眠一定时间，保证两个消息源所在线程能正常执行完毕
         OperatorUtils.sleep(5000)
+    }
+
+    @Test
+    fun test_buffer_1() {
+        val testSubscriber = TestSubscriber<List<String>>()
+
+        //缓冲2个发射一次
+        Flowable.just("A", "B", "C", "D")
+            .buffer(2)
+            .subscribe(testSubscriber)
+
+        testSubscriber.assertResult(listOf("A", "B"), listOf("C", "D"))
+        testSubscriber.assertValueCount(2)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_timer_2() {
+        val testSubscriber = TestSubscriber<Long>()
+        //延时5秒发射
+        Flowable.timer(5, TimeUnit.SECONDS, mTestScheduler)
+            .subscribe(testSubscriber)
+
+        //时间到5秒
+        mTestScheduler.advanceTimeTo(5, TimeUnit.SECONDS)
+        testSubscriber.assertValue(0L)
+        testSubscriber.assertValueCount(1)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_interval_2() {
+        val testSubscriber = TestSubscriber<Long>()
+
+        //隔1秒发射一次，一共10次
+        Flowable.interval(1, TimeUnit.SECONDS, mTestScheduler)
+            .take(10)
+            .subscribe(testSubscriber)
+
+        //时间经过3秒
+        mTestScheduler.advanceTimeBy(3, TimeUnit.SECONDS)
+        testSubscriber.assertValues(0L, 1L, 2L)
+        testSubscriber.assertValueCount(3)
+        testSubscriber.assertNotComplete()
+
+        //时间再经过2秒
+        mTestScheduler.advanceTimeBy(2, TimeUnit.SECONDS)
+        testSubscriber.assertValues(0L, 1L, 2L, 3L, 4L)
+        testSubscriber.assertValueCount(5)
+        testSubscriber.assertNotComplete()
+
+        //时间到10秒
+        mTestScheduler.advanceTimeTo(10, TimeUnit.SECONDS)
+        testSubscriber.assertValueCount(10)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_buffer_2() {
+        val testSubscriber = TestSubscriber<List<String>>()
+
+        //缓冲2个发射一次
+        Flowable.just("A", "B", "C", "D")
+            .buffer(2)
+            .subscribe(testSubscriber)
+
+        testSubscriber.assertResult(listOf("A", "B"), listOf("C", "D"))
+        testSubscriber.assertValueCount(2)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_repeat_2() {
+        val testSubscriber = TestSubscriber<Int>()
+        Flowable.fromIterable(listOf(1, 2))
+            .repeat(2) //重复发送2次
+            .subscribe(testSubscriber)
+
+        testSubscriber.assertValues(1, 2, 1, 2)
+        testSubscriber.assertValueCount(4)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_range_2() {
+        val testSubscriber = TestSubscriber<Int>()
+
+        //从3开始发射3个连续的int
+        Flowable.range(3, 3).subscribe(testSubscriber)
+
+        testSubscriber.assertValues(3, 4, 5)
+        testSubscriber.assertValueCount(3)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_from_2() {
+        val testSubscriber = TestSubscriber<Int>()
+
+        //依次发射list中的数
+        Flowable.fromIterable(listOf(1, 2)).subscribe(testSubscriber)
+
+        testSubscriber.assertValues(1, 2)
+        testSubscriber.assertValueCount(2)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_error_2() {
+        val testSubscriber = TestSubscriber<Any>()
+        val exception = RuntimeException("error")
+        Flowable.error<Any>(exception).subscribe(testSubscriber)
+        testSubscriber.assertError(exception)
+        testSubscriber.assertError {
+            it.message == "error"
+        }
+    }
+
+    @Test
+    fun test_just_2() {
+        val testSubscriber = TestSubscriber<String>()
+
+        //依次发射A，B，C
+        Flowable.just("A", "B", "C").subscribe(testSubscriber)
+
+        //断言值是否相等
+        testSubscriber.assertValues("A", "B", "C")
+
+        //断言值的数量是否相等
+        testSubscriber.assertValueCount(3)
+        testSubscriber.assertComplete()
+    }
+
+    @Test
+    fun test_observer_2() {
+        val testObserver = TestObserver.create<Int>()
+        testObserver.onNext(1)
+        testObserver.onNext(2)
+        testObserver.onComplete()
+
+        //断言值是否相等
+        testObserver.assertValues(1, 2)
+
+        //断言是否完成
+        testObserver.assertComplete()
     }
 }
